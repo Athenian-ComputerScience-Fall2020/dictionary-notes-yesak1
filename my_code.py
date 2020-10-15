3fb6d7f7ac75f3b206a54d06cddec763f0827877
yesak1 <23kkazliner@athenian.org>
2020-10-15 16:42:36 +0000

    done

--- my_code.py
+++ my_code.py
@@ -1,2 +1,28 @@
 # Use this to take notes on the Edpuzzle video. Try each example rather than just watching it - you will get much more out of it!
-#  
+#  Most things are commented out because they can't all coexist without a syntax error
+
+user = {"name": "Kasey", "age": 15, "courses": ["History, CompSci"]}
+
+for key, value in user.items():
+    print(key, value)
+
+#print(user.items())
+
+#print(user.values())
+
+#print(user.keys())
+
+#print(len(user))
+
+#age=user.pop("age")
+
+# del user['age']
+
+'''user.update({"name": "Bob", "age": 25, "phone": "888-8888"})
+user['phone'] = '888-8888'
+user['name']='Bob'''''
+
+#print(user.get("age",'not found'))
+
+#print(user)
+#print(age)
